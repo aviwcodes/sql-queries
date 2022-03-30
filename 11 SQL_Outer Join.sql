5c7a269f2e583f6ae46d692b9d7ee90eae40baa7
AviW Codes <99354032+aviwcodes@users.noreply.github.com>
2022-03-31 00:14:34 +0530

    outer join queries

---
+++ 11 SQL_Outer Join.sql
@@ -0,0 +1,62 @@
+use database dev_db;
+use schema staging;
+
+create or replace table t1 (col1 integer);
+create or replace table t2 (col1 integer);
+insert into t1 (col1) values (2),(3),(4);
+insert into t2 (col1) values (1),(2),(2),(3);
+
+select t1.col1 as t1_col1, t2.col1 as t2_col1
+    from t1 left outer join t2
+        on t2.col1 = t1.col1
+    order by t1_col1,t2_col1;
+
+
+select t1.col1 as t1_col1, t2.col1 as t2_col1
+    from t1 right outer join t2
+        on t2.col1 = t1.col1
+    order by t1_col1,t2_col1;
+
+
+select t1.col1 as t1_col1, t2.col1 as t2_col1
+    from t1 full outer join t2
+        on t2.col1 = t1.col1
+    order by t1_col1,t2_col1;
+
+
+create or replace table employee(id int, empname varchar(50),city varchar(10),deptid int);
+create or replace table department(deptid int, deptname varchar(50));
+
+insert into employee(id,empname,city,deptid)
+values(10,  'Ram',    'Pune',       100),
+      (11,  'Jake',   'Mumbai',     200),
+      (12,  'Tom',    'Mumbai',     200),
+      (13,  'Jerry',  'Hydrabad',   300);
+
+
+insert into department(deptid,deptname)
+values(100, 'Marketing'),
+      (200, 'HR'),
+      (400, 'Sales'),
+      (500, 'Purchase');
+
+--Left outer join
+select id, empname,e.deptid, deptname
+from employee e left outer join department d
+using (deptid);
+
+--Right outer join
+select id, empname,d.deptid, deptname
+from employee e right outer join department d
+using (deptid);
+
+
+--full outer join 
+select id, empname,e.deptid as E_deptid,d.deptid as d_deptid, deptname
+from employee e full outer join department d
+using (deptid);
+
+select d.deptid,deptname, count(e.deptid) as Count_of_emps
+from employee e right join department d
+using(deptid)
+group by d.deptid,deptname;
